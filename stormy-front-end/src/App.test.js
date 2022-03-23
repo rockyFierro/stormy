8b587f21e4a3dd8a7c513d61f0dd8fe600cfef7b
Rocky Fierro <finnslighthouse@gmail.com>
2022-03-23 14:56:35 -0700

    testing set-up complete.

--- stormy-front-end/src/App.test.js
+++ stormy-front-end/src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Madness scale.", ()=>{
+  it("Ensures I code slowly and thoughtfully.", () => {
+    expect("One of us only tells the truth and the other only writes tests").toMatch(/truth/)
+  })
+})
