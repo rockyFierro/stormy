e14b31ecf699c72596225a23542e1dfccad89f8c
Rocky Fierro <finnslighthouse@gmail.com>
2022-03-23 18:55:17 -0700

    using hooks; find geolocation; set lat and long.

--- stormy-front-end/src/App.js
+++ stormy-front-end/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 function App() {
+  
+  const [ lat, setLat ] = useState([])
+  const [ long, setLong ] = useState([])
+
+  useEffect( () => {
+    // collect information for location.
+    navigator.geolocation.getCurrentPosition( function(position){
+      setLat(position.coords.latitude)
+      setLong(position.coords.longitude)
+    } )
+
+    
+    console.log(lat, long)
+  }, [lat, long])
+
   return (
     <div className="App">
     </div>
   );
 }
 
-export default App;
+export default App
